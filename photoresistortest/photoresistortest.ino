15dd44fbaf9abbf0f89ba9613a396d0f68ba6dca
btgwynn77089 <btgwynn77089@gmail.com>
2023-10-31 18:58:58 -0500

    Photoresistor detects change now

--- photoresistortest/photoresistortest.ino
+++ photoresistortest/photoresistortest.ino
@@ -1,13 +1,39 @@
 #define RESISTOR_PIN A0
+int prValCurrent = 0;
+int prValPrev = 0;
+int difference = 0;
+bool chng = true;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(RESISTOR_PIN, INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(analogRead(RESISTOR_PIN));
-  delay(100);
+  prValPrev = prValCurrent;
+  prValCurrent = analogRead(RESISTOR_PIN);
+  difference = prValCurrent - prValPrev;
+  if (difference < 0) {
+    difference *= -1;
+  }
+  if (difference > 10) {
+    chng = true;
+  }
+  else{
+    chng = false;
+  }
+  if (chng == true) {
+    Serial.println("Change Detected");
+    Serial.println(difference);
+  }
+  else {
+    Serial.println("No Change");
+    Serial.println(difference);
+  }
+  
+
+  delay(2000);
 }
--- sketch_sep12a/sketch_sep12a.ino
+++ sketch_sep12a/sketch_sep12a.ino
@@ -1,26 +1,24 @@
 int motorPin1 = 2;
 int motorPin2 = 4;
 
-int photoPin = A0;
+#define photoPin = A0;
 
 int photoInput = 7;
 
 void setup() {
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-  pinMode(photoInput, OUTPUT);
 
   pinMode(photoPin, INPUT);
-  }
 
-void loop() {
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, HIGH);
-  digitalWrite(photoInput, HIGH);
-  
+  }
+
+void loop() {  
   int value = analogRead(photoPin);
 
-  if (value < 10){
+  if (value < 10) {
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, LOW);
     }
