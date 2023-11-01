9fa8f5dee046a2f21a18b7923840cc175be1bb55
nathansamuell <nathansamuell@duck.com>
2023-11-01 18:21:15 -0500

    added motor control functionality

--- sketch_sep12a/sketch_sep12a.ino
+++ sketch_sep12a/sketch_sep12a.ino
@@ -1,30 +1,66 @@
-int motorPin1 = 2;
-int motorPin2 = 4;
+// Nathan Samuell and Benjamin Gwynn
+// UH CHEMECAR 2023
+// currently testing the full motor functionaliy
 
-int photoPin = A0;
+
+//instantiation
+int motorPin1 = 2; // motor pins
+int motorPin2 = 4;
+int motorPin3 = 8;
+int motorPin4 = 12;
+int photoPin = A0; // pin to read photoresistor input
+int lightcheck = 100; // variable used as light input threshold during testing
+bool isOn = false; // used in the motor control method
+
 
 void setup() {
+  //pinmode setup
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
-
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
   pinMode(photoPin, INPUT);
-
-  Serial.begin(9600);
+  Serial.begin(9600); //start serial monitor for monitoring arduino output, can be reached by going to tools > serial monitor
   }
 
-void loop() {  
-  int value = analogRead(photoPin);
-  digitalWrite(motorPin1, HIGH);
-  digitalWrite(motorPin2, HIGH);
 
-  if (value < 50) {
+//turns the motors on or off based on a true/false input
+void turnMotors(String onOrOff) {
+  if (onOrOff == "on") {
+    isOn = true;
+  }
+  else {
+    isOn = false;
+  }
+  if (isOn == true) {
+    digitalWrite(motorPin1, HIGH);
+    digitalWrite(motorPin2, HIGH);
+    digitalWrite(motorPin3, HIGH);
+    digitalWrite(motorPin4, HIGH);
+  }
+  else {
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, LOW);
-    Serial.println("dark");
-    while(1);
+    digitalWrite(motorPin3, LOW);
+    digitalWrite(motorPin4, LOW);
   }
-  if (value > 50) {
-    Serial.println("light");
+}
+
+void loop() {  
+  int value = analogRead(photoPin); // read photoresistor value
+  turnMotors("on"); // turn the motors on by default
+
+  if (value < lightcheck) { // check to see if motors should be off
+    turnMotors("off");
+    String output = "dark -- ";
+    Serial.println(output); // debug output
+    while(1); //forever loop to break the cycle, reset process manually for now using arduino reset button (located by the serial port)
   }
-  delay(1000);
+  if (value > lightcheck) {
+    String output = "light -- ";
+    Serial.println(output); // print light if the motors should be on
+  }
+  delay(1000); // check once per second
+  Serial.println(value);
+  
   }
