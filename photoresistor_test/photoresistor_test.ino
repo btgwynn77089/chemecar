6e722d5d0991c44d72fd6616c11c3914713a70af
nathansamuell <nathansamuell@duck.com>
2023-11-02 16:56:43 -0500

    working on PR test

--- photoresistor_test/photoresistor_test.ino
+++ photoresistor_test/photoresistor_test.ino
@@ -1,3 +1,13 @@
+// Nathan Samuell
+// 2 Nov 2023
+// CHEMECAR 2023
+
+// tests the functionality of the photoresistor component in the full car
+// designed to be ran in a script sequence from an external computer using arduino cli
+
+int photoIn = A0;
+
+
 void setup() {
   // put your setup code here, to run once:
 
