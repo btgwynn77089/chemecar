d6e45958a4df80b79b44a9f96c8e3c80b02863c2
nathansamuell <nathansamuell@duck.com>
2023-11-09 17:12:59 -0600

    added motor test script -- documentation to come later

--- motors_test/motors_test.ino
+++ motors_test/motors_test.ino
@@ -2,8 +2,50 @@
 //used to test car with wheels actually on it. any insights will be added into the prototype file.
 
 
+//instantiation
+int motorPin1 = 2; // motor pins
+int motorPin2 = 4;
+int motorPin3 = 8;
+int motorPin4 = 12;
+
 void setup() {
-  // put your setup code here, to run once:
+  // set motorpins as out
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+
+  digitalWrite(motorPin1,HIGH);
+  delay(3000);
+  digitalWrite(motorPin1,LOW);
+  delay(3000);
+
+  digitalWrite(motorPin2,HIGH);
+  delay(3000);
+  digitalWrite(motorPin2,LOW);
+  delay(3000);
+
+  digitalWrite(motorPin3,HIGH);
+  delay(3000);
+  digitalWrite(motorPin3,LOW);
+  delay(3000);
+
+  digitalWrite(motorPin4,HIGH);
+  delay(3000);
+  digitalWrite(motorPin4,LOW);
+  delay(3000);
+
+
+  digitalWrite(motorPin1,HIGH);
+  digitalWrite(motorPin2,HIGH);
+  digitalWrite(motorPin3,HIGH);
+  digitalWrite(motorPin4,HIGH);
+  delay(5000);
+  
+  digitalWrite(motorPin1,LOW);
+  digitalWrite(motorPin2,LOW);
+  digitalWrite(motorPin3,LOW);
+  digitalWrite(motorPin4,LOW);
 
 }
 
