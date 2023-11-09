d37826e54e1c2d71884ba5d6c94220cef5ff67c7
nathansamuell <nathansamuell@duck.com>
2023-11-09 16:26:19 -0600

    started work on motor test script

---
+++ motors_test/motors_test.ino
@@ -0,0 +1,13 @@
+//Nathan Samuell
+//used to test car with wheels actually on it. any insights will be added into the prototype file.
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
